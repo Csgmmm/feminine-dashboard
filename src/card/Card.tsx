7376fa1962ad25fc238981791626699febf7cd49
Carla Sofia Martins <carla.s.ofia@hotmail.com>
2026-03-23 17:50:23 +0000

    Edited profile, adding Component Card to it

--- src/card/Card.tsx
+++ src/card/Card.tsx
@@ -4,12 +4,13 @@ import styles from "./card.module.css";
 interface ICard {
   children: ReactNode;
   variant?: "default" | "upgrade";
+  className?: string;
 }
 
-function Card({ children, variant = "default" }: ICard) {
+function Card({ children, variant = "default", className }: ICard) {
   return (
     <div
-      className={`${styles.card} ${variant === "upgrade" ? styles.upgradeCard : ""}`}
+      className={`${styles.card} ${variant === "upgrade" ? styles.upgradeCard : ""} ${className}`}
     >
       {children}
     </div>
