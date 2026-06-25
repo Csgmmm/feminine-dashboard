a0a81284627d412399be21c1ffe3b878d73a0dca
Carla Sofia Martins <carla.s.ofia@hotmail.com>
2026-06-25 20:23:08 +0100

    updated login

--- src/login/Login.tsx
+++ src/login/Login.tsx
@@ -36,7 +36,7 @@ const Login = () => {
           <div className={styles.leftSide}>
             <Logo />
             <div className={styles.titleInput}>
-              <h2 className={styles.title}>Welcome to MyCicle</h2>
+              <h2 className={styles.title}>Welcome to MyCycle</h2>
               <h3 className={styles.subtitle}>This is your safe place</h3>
             </div>
 
